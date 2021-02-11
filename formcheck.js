2ea81744c709f3a63d0abd1f111fcd51643d7ec9
Bergan16 <74006739+Bergan16@users.noreply.github.com>
2021-02-11 19:36:31 +0100

    index och js fil

---
+++ formcheck.js
@@ -0,0 +1,8 @@
+//funktion som lagrar talet i variabeln "Ar" som sedan används för att skriva ut när man är född"
+function getAr(){
+	var Ar =document.getElementById("Ar").value;
+	document.getElementById("utskrift").innerHTML= "Du är född " +(2021-Ar);
+}
+ 
+
+
